08484037c67c85236e8f68030a3a7744658b4349
Anastasiiasyvak <asyvak@kse.org.ua>
2023-12-19 23:39:24 +0200

    add screen and points classes

--- main.cpp
+++ main.cpp
@@ -50,7 +50,6 @@ public:
     }
 };
 
-
 class Paddle{
 protected:
     sf::RectangleShape paddle;
@@ -97,11 +96,72 @@ public:
     : Paddle(10.0f, 150.0f, sf::Color(186, 22, 63), 25.0f, 720 / 2.0f, 0.25f){}
 };
 
-
 class UserPaddle: public Paddle{
 public:
     UserPaddle()
     : Paddle(10.0f, 150.0f, sf::Color(186, 22, 63), 1280 - 25.0f, 720 / 2.0f, 0.25f){}
+};
+
+class Points{
+private:
+    int userScore;
+    int botScore;
+public:
+    Points() : userScore(0), botScore(0){}
+
+    void increaseUserScore(){
+        userScore++;
+    }
+
+    void increaseBotScore(){
+        botScore++;
+    }
+
+    int getUserScore() const{
+        return userScore;
+    }
+
+    int getBotScore() const{
+        return botScore;
+    }
+};
+
+class Screen{
+private:
+    sf::VideoMode videoMode;
+    sf::RenderWindow window;
+public:
+    Screen(int width, int height, const std::string& title)
+    : videoMode(width, height), window(videoMode, title){}
+
+    void clear(const sf::Color& color){
+        window.clear(color);
+    }
+
+    void draw(const sf::Drawable& drawable){
+        window.draw(drawable);
+    }
+
+    void display(){
+        window.display();
+    }
+
+    bool isOpen() const {
+        return window.isOpen();
+    }
+
+    bool pollEvent(sf::Event& event){
+        return window.pollEvent(event);
+    }
+
+    sf::VideoMode getVideoMode() const{
+        return videoMode;
+    }
+
+    sf::RenderWindow& getWindow() {
+        return window;
+    }
+
 };
 
 
@@ -109,43 +169,32 @@ class PongGame{
 
 };
 
-class Points{
-
-};
-
-class User{
-
-};
-
-class Screen{
-
-};
 
 
-class Message{
 
-};
+
+
+
+
 
 
 
 int main() {
-    sf::VideoMode VM(1280, 720);
-    sf::RenderWindow window(VM, "Pong game by Nastusha");
+    Screen screen(1280, 720, "Pong game by Nastusha");
     sf::Color backgroundColor(51, 102, 200);
 
     Ball ball;
     BotPaddle botPaddle;
     UserPaddle userPaddle;
+    Points points;
 
-
-
-    sf::RectangleShape verticalLine(sf::Vector2f(2.0f, VM.height));
+    sf::RectangleShape verticalLine(sf::Vector2f(2.0f, screen.getVideoMode().height));
     verticalLine.setFillColor(sf::Color::White);
-    verticalLine.setPosition(VM.width / 2.0f, 0.0f);
+    verticalLine.setPosition(screen.getVideoMode().width / 2.0f, 0.0f);
 
-    sf::RectangleShape horizontalLine(sf::Vector2f(VM.width, 2.0f));
+    sf::RectangleShape horizontalLine(sf::Vector2f(screen.getVideoMode().width, 2.0f));
     horizontalLine.setFillColor(sf::Color::White);
-    horizontalLine.setPosition(0.0f, VM.height / 2.0f);
+    horizontalLine.setPosition(0.0f, screen.getVideoMode().height / 2.0f);
 
 
 
@@ -159,7 +208,7 @@ int main() {
     menuText.setCharacterSize(40);
     menuText.setFillColor(sf::Color::White);
     menuText.setString("Press 1 for a short game (11 points)\nPress 2 for a long game (21 points)");
-    menuText.setPosition(VM.width / 2.0f - 300.0f, VM.height / 2.0f - 50.0f);
+    menuText.setPosition(screen.getVideoMode().width / 2.0f - 300.0f, screen.getVideoMode().height / 2.0f - 50.0f);
 
     int gamePoints = 0;
 
@@ -167,33 +216,30 @@ int main() {
     scoreText.setFont(font);
     scoreText.setCharacterSize(30);
     scoreText.setFillColor(sf::Color::White);
-    scoreText.setPosition(VM.width / 2.0f - 20.0f, 10.0f);
+    scoreText.setPosition(screen.getVideoMode().width / 2.0f - 20.0f, 10.0f);
 
     sf::Text resultText;
     resultText.setFont(font);
     resultText.setCharacterSize(40);
     resultText.setFillColor(sf::Color::White);
-    resultText.setPosition(VM.width / 2.0f, VM.height / 2.0f);
+    resultText.setPosition(screen.getVideoMode().width / 2.0f, screen.getVideoMode().height / 2.0f);
 
     sf::Text restartText;
     restartText.setFont(font);
     restartText.setCharacterSize(30);
     restartText.setFillColor(sf::Color::White);
     restartText.setString("Press SPACE to play again or Esc for close the game");
-    restartText.setPosition(VM.width / 2.0f - 200.0f, VM.height / 2.0f + 100.0f);
-
-    int userScore = 0;
-    int botScore = 0;
+    restartText.setPosition(screen.getVideoMode().width / 2.0f - 200.0f, screen.getVideoMode().height / 2.0f + 100.0f);
 
     bool showMenu = true;
     bool isGameOver = false;
     bool restartRequested = false;
 
-    while (window.isOpen()) {
+    while (screen.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event)) {  // Функція pollEvent викликає обробку однієї події за кожний виклик циклу
+        while (screen.pollEvent(event)) {  // Функція pollEvent викликає обробку однієї події за кожний виклик циклу
             if (event.type == sf::Event::Closed) {
-                window.close();
+                screen.getWindow().close();
             } else if (event.type == sf::Event::KeyPressed) {
                 if (showMenu) {
                     if (event.key.code == sf::Keyboard::Num1) {
@@ -204,7 +250,7 @@ int main() {
                         showMenu = false;
                     }
                 } else if (event.key.code == sf::Keyboard::Escape) {
-                    window.close();
+                    screen.getWindow().close();
                 } else if (event.key.code == sf::Keyboard::Space && isGameOver) {
                     restartRequested = true;
                 }
@@ -212,18 +258,17 @@ int main() {
         }
 
         if (restartRequested) {
-            userScore = 0;
-            botScore = 0;
+            points = Points();
             isGameOver = false;
             showMenu = true;
             restartRequested = false;
             scoreText.setString("0 : 0");
         }
 
-        window.clear(backgroundColor);
+        screen.clear(backgroundColor);
 
         if (showMenu) {
-            window.draw(menuText);
+            screen.draw(menuText);
         } else {
             if (!isGameOver) {
                 ball.move();
@@ -247,51 +292,51 @@ int main() {
                 }
 
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) &&
-                    userPaddle.getPositionY() + userPaddle.getSizeY() < VM.height) {
+                    userPaddle.getPositionY() + userPaddle.getSizeY() < screen.getVideoMode().height) {
                     userPaddle.moveDown();
                 }
 
-                if (ball.getPositionX() + (2 * ball.getRadius()) > VM.width || ball.getPositionX() < 0) {
+                if (ball.getPositionX() + (2 * ball.getRadius()) > screen.getVideoMode().width || ball.getPositionX() < 0) {
                     ball.reverseSpeedX();
 
                     if (ball.getPositionX() < 0) {
-                        botScore++;
+                        points.increaseBotScore();
                     } else {
-                        userScore++;
+                        points.increaseUserScore();
                     }
 
-                    scoreText.setString(std::to_string(userScore) + " : " + std::to_string(botScore));
+                    scoreText.setString(std::to_string(points.getUserScore()) + " : " + std::to_string(points.getBotScore()));
                 }
 
-                if (userScore == gamePoints || botScore == gamePoints) {
+                if (points.getUserScore() == gamePoints || points.getBotScore() == gamePoints) {
                     isGameOver = true;
 
-                    if (userScore == gamePoints) {
+                    if (points.getUserScore() == gamePoints) {
                         resultText.setString("You lose!");
-                    } else if (botScore == gamePoints) {
+                    } else if (points.getBotScore() == gamePoints) {
                         resultText.setString("You win!");
                     }
                 }
 
-                if (ball.getPositionY() + (2 * ball.getRadius()) > VM.height || ball.getPositionY() < 0) {
+                if (ball.getPositionY() + (2 * ball.getRadius()) > screen.getVideoMode().height || ball.getPositionY() < 0) {
                     ball.reverseSpeedY();
                 }
             }
 
-            window.draw(verticalLine);
-            window.draw(horizontalLine);
-            window.draw(ball.getShape());
-            window.draw(botPaddle.getShape());
-            window.draw(userPaddle.getShape());
-            window.draw(scoreText);
+            screen.draw(verticalLine);
+            screen.draw(horizontalLine);
+            screen.draw(ball.getShape());
+            screen.draw(botPaddle.getShape());
+            screen.draw(userPaddle.getShape());
+            screen.draw(scoreText);
 
             if (isGameOver) {
-                window.draw(resultText);
-                window.draw(restartText);
+                screen.draw(resultText);
+                screen.draw(restartText);
             }
         }
 
-        window.display();
+        screen.display();
     }
 
     return 0;
